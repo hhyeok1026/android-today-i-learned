15024cc42efed71ce054ab3c1fc5f75f9036af5a
hhyeok1026 <hhyeok1026@gmail.com>
2022-12-22 13:41:29 +0900

    Gradle with Kotlin script (ok)

---
+++ frozencoder-inflearn/BookSearchApp/buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,38 @@
+object Versions {
+
+    // Project
+    const val AGP = "7.3.1"
+    const val KOTLIN = "1.7.20"
+    const val SECRETS_GRADLE = "2.0.1"
+
+    // Androidx
+    const val CORE_KTX = "1.9.0"
+    const val APP_COMPAT = "1.5.1"
+    const val MATERIAL = "1.7.0"
+    const val CONSTRAINT_LAYOUT = "2.1.4"
+
+    // Testing
+    const val JUNIT4 = "4.13.2"
+    const val ANDROID_JUNIT = "1.1.4"
+    const val ESPRESSO_CORE = "3.5.0"
+
+    // Library
+    const val RETROFIT = "2.9.0"
+    const val MOSHI = "1.13.0"
+    const val OKHTTP = "4.10.0"
+    const val LIFECYCLE = "2.5.1"
+    const val COROUTINE = "1.6.1"
+    const val COROUTINE_ANDROID = "1.6.4"
+    const val COIL = "2.2.2"
+    const val RECYCLERVIEW = "1.2.1"
+    const val NAVIGATION = "2.5.3"
+    const val ROOM = "2.4.3"
+    const val KOTLIN_SERIALIZATION = "1.4.1"
+    const val DATASTORE = "1.0.0"
+    const val PAGING = "3.1.1"
+    const val WORKMANGER = "2.7.1"
+    const val HILT = "2.44"
+    const val ACTIVITY_KTX = "1.6.1"
+    const val FRAGMENT_KTX = "1.5.5"
+    const val HILT_EXTENSION = "1.0.0"
+}
